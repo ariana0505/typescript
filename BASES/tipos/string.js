a8a52776a215ad915ab6a9c25e3fc1fc5f36e44e
ariana0505 <penaariana075@gmail.com>
2025-02-18 00:58:44 -0500

    tipo string

---
+++ BASES/tipos/string.js
@@ -0,0 +1,7 @@
+"use strict";
+(() => {
+    const batman = 'Batman';
+    const linternaVerde = `linternaVerde`;
+    const vol = "Hola 'joven' c:";
+    console.log(vol);
+});
---
+++ BASES/tipos/string.ts
@@ -0,0 +1,7 @@
+(()=>{
+    const batman:string = 'Batman';
+    const linternaVerde:string = `linternaVerde`;
+    const vol:string = "Hola 'joven' c:"
+    
+    console.log(vol);
+})
