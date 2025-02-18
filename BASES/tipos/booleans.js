aca3c01e6ee43b0b6df58b2365380f5f40970fff
ariana0505 <penaariana075@gmail.com>
2025-02-17 22:40:29 -0500

    buleanos

--- BASES/tipos/booleans.js
+++ BASES/tipos/booleans.js
@@ -1,5 +1,7 @@
 "use strict";
 (() => {
     let isSurperman = true;
+    let isBatman = false;
+    isSurperman = (isBatman) ? true : false;
     console.log(typeof isSurperman);
 })();
--- BASES/tipos/booleans.ts
+++ BASES/tipos/booleans.ts
@@ -1,4 +1,8 @@
 (()=>{
       let isSurperman:boolean = true;
+      let isBatman:boolean = false
+      
+      isSurperman = (isBatman) ? true:false
       console.log(typeof isSurperman);
+
 })()
